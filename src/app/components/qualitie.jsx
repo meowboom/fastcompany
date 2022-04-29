b6bcb997f2bdf3217bb37c4a3f6a30d743b79d8e
Maksim Dzinisiuk <csgomeow@gmail.com>
2022-04-29 19:54:12 +0200

    pagination

--- src/app/components/qualitie.jsx
+++ src/app/components/qualitie.jsx
@@ -2,16 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Qualitie = (props) => {
-  const { _id, name, color } = props;
-  return (
-    <span className={"badge m-1 bg-" + color} key={_id}>
-      {name}
-    </span>
-  );
+  const { name, color } = props;
+  return <span className={"badge m-1 bg-" + color}>{name}</span>;
 };
 
 Qualitie.protoType = {
-  _id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   color: PropTypes.string.isRequired,
 };
